83a2898207041f42517a795713ede3fae5503bdf
sunmin-oss <c112152342@nkust.edu.tw>
2026-01-12 22:15:20 +0800

    Set base path for Vite configuration
    Add base path configuration for deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/tokyo-trip/",  // <--- 關鍵！一定要有這一行，且前後都要有斜線
 })
